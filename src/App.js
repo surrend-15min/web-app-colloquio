139f4ec49ac6703f724876769ea4f9cecafed17e
FagianoRampante <56256292+surrend-15min@users.noreply.github.com>
2024-02-25 17:01:07 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,69 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const App = () => {
-  const [apiData, setApiData] = useState(null);
+function App() {
+  const [data, setData] = useState(null);
 
-  const fetchData = async () => {
-    try {
-      const response1 = await fetch('/api1'); // Utilizzo del proxy per la prima API
-      const data = await response1.json();
-      // Parsing della stringa JSON contenuta nel campo body
-      data.body = JSON.parse(data.body);
-      setApiData(data);
-    } catch (error) {
-      console.error('Errore durante il recupero dei dati:', error);
-    }
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('https://89nutx8pxe.execute-api.eu-central-1.amazonaws.com/dev');
+      setData(result.data);
+    };
 
-  const handleSecondApiButtonClick = async (str, index) => {
-    try {
-      console.log(`Hai cliccato sul bottone per: ${str}`);
-      const response = await fetch(`/api2/?body=${encodeURIComponent(str)}`);
-      const data = await response.json();
-      
-      // Analizziamo correttamente il corpo della risposta JSON
-      const body = JSON.parse(data.body);
-      
-      // Estraiamo il link s3Link dalla risposta
-      const s3Link = body.s3Link;
-      
-      // Costruiamo il link utilizzando la stringa ottenuta dalla risposta
-      const linkElement = <a href={s3Link} target="_blank" rel="noopener noreferrer">{str}</a>;
-      
-      // Aggiorniamo la lista di stringhe con il nuovo link
-      setApiData(prevData => {
-        const newData = [...prevData.body];
-        newData[index] = linkElement;
-        return { ...prevData, body: newData };
-      });
-    } catch (error) {
-      console.error('Errore durante la richiesta all\'API:', error);
-    }
-  };
-  
-  
-  
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Dati dall'API</h1>
-      <button onClick={fetchData}>Carica Dati dall'API</button>
-      {apiData && (
-        <div>
-          <p>StatusCode: {apiData.statusCode}</p>
-          <h2>Elenco delle stringhe:</h2>
-          <ul>
-            {apiData.body.map((str, index) => (
-              <li key={index}>
-                {str}
-                <button onClick={() => handleSecondApiButtonClick(str, index)}>Secondo Bottone</button>
-
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      {data && <p>{data}</p>}
     </div>
   );
-};
+}
 
 export default App;
